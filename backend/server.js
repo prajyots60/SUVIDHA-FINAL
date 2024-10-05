3f39c02b0518ad169252afbfc2cc9515a3917a58
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-10-05 13:24:05 +0530

    Refactor server.js to serve static files in production

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,15 @@ const dropEmailIndex = async () => {
   }
 };
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+
 app.listen(PORT, async () => {
   await connectDB(); // Connect to the database
   await dropEmailIndex(); // Drop the email index before starting the server
