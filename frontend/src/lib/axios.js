5f110d64609905d945253dacb1be97e75f11b3bf
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-10-05 13:05:41 +0530

    Refactor axios interceptor and update login and signup page styles

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: import.meta.NODE_ENV === "development" ? "http://localhost:3000/api" : "/api", // Adjust the URL based on the environment
+  baseURL: import.meta.NODE_ENV === "development" ? "http://localhost:3000/api" : "/api", // Adjust the URL based on the environment
   // baseURL: import.meta.env.VITE_ENV === "development" ? "http://localhost:3000/api" : "/api",
 
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
 
-  baseURL: "http://localhost:3000/api", // Adjust the URL based on the environment
+  // baseURL: "http://localhost:3000/api", // Adjust the URL based on the environment
   withCredentials: true,  // send cookies with requests
 });
 
